4e0b041a1af8433003cdcb2e8f772b8a8656f7a9
Jahmandee <amandalaneux@gmail.com>
2022-11-22 22:46:58 +1000

    Lesson Three Practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+
 
 function App() {
+  const [task, setTask] = useState({
+    id: 0,
+    taskDescription: "",
+    isCompleted: false,
+  });
+
+  const [todos, setTodos] = useState([]);
+
+  function addTodos(e) {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        taskDescription: task.taskDescription,
+        isCompleted: false,
+      },
+    ]);
+
+    setTask({
+      id: 0,
+      taskDescription: "",
+      isCompleted: false,
+    });
+  }
+
+  // id: 0,
+  //   taskDescription: "",
+  //   isCompleted: false,
+
+  function handleCompleteButton(id) {
+    const mapped = todos.map((task) => {
+      return task.id === Number(id)
+        ? {
+            ...task,
+            isCompleted: true,
+          }
+        : { ...task };
+    });
+
+    setTodos(mapped);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello React World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <form onSubmit={addTodos}>
+        <div>
+          <label>
+            Task
+            <input
+              type="text"
+              name="taskDescription"
+              onChange={(event) =>
+                setTask({
+                  taskDescription: event.target.value,
+                  isCompleted: false,
+                })
+              }
+            />
+          </label>
+          <button>Add Todo</button>
+        </div>
+      </form>
+
+      {todos.map((todo, index) => {
+        return (
+          <div>
+            <div key={todo.id + index}>
+              {todo.isCompleted ? (
+                <strike>{todo.taskDescription}</strike>
+              ) : (
+                <p>{todo.taskDescription}</p>
+              )}
+            </div>
+            <button onClick={() => handleCompleteButton(todo.id)}>
+              Complete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
